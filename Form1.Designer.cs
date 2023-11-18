2fec66c1983dedcf200e51f9e9ad7690b53ba2ad
caganniflehim <omercagand@gmail.com>
2023-11-18 17:04:41 +0300

    Initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,155 @@
+﻿
+namespace dersform
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lblsonuc = new System.Windows.Forms.Label();
+            this.txtvize = new System.Windows.Forms.TextBox();
+            this.txtfinal = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lblorta = new System.Windows.Forms.Label();
+            this.lbldurum = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(46, 99);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(39, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "vize ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(46, 140);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(38, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "final";
+            // 
+            // lblsonuc
+            // 
+            this.lblsonuc.AutoSize = true;
+            this.lblsonuc.Location = new System.Drawing.Point(264, 99);
+            this.lblsonuc.Name = "lblsonuc";
+            this.lblsonuc.Size = new System.Drawing.Size(69, 20);
+            this.lblsonuc.TabIndex = 2;
+            this.lblsonuc.Text = "ortalama";
+            // 
+            // txtvize
+            // 
+            this.txtvize.Location = new System.Drawing.Point(115, 92);
+            this.txtvize.Name = "txtvize";
+            this.txtvize.Size = new System.Drawing.Size(125, 27);
+            this.txtvize.TabIndex = 3;
+            // 
+            // txtfinal
+            // 
+            this.txtfinal.Location = new System.Drawing.Point(115, 133);
+            this.txtfinal.Name = "txtfinal";
+            this.txtfinal.Size = new System.Drawing.Size(125, 27);
+            this.txtfinal.TabIndex = 4;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(115, 189);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(94, 29);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "hesapla";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(264, 140);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(52, 20);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "durum";
+            // 
+            // lblorta
+            // 
+            this.lblorta.AutoSize = true;
+            this.lblorta.Location = new System.Drawing.Point(362, 95);
+            this.lblorta.Name = "lblorta";
+            this.lblorta.Size = new System.Drawing.Size(50, 20);
+            this.lblorta.TabIndex = 7;
+            this.lblorta.Text = "label4";
+            // 
+            // lbldurum
+            // 
+            this.lbldurum.AutoSize = true;
+            this.lbldurum.Location = new System.Drawing.Point(344, 140);
+            this.lbldurum.Name = "lbldurum";
+            this.lbldurum.Size = new System.Drawing.Size(50, 20);
+            this.lbldurum.TabIndex = 8;
+            this.lbldurum.Text = "label5";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lbldurum);
+            this.Controls.Add(this.lblorta);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txtfinal);
+            this.Controls.Add(this.txtvize);
+            this.Controls.Add(this.lblsonuc);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lblsonuc;
+        private System.Windows.Forms.TextBox txtvize;
+        private System.Windows.Forms.TextBox txtfinal;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label lblorta;
+        private System.Windows.Forms.Label lbldurum;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace dersform
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //bu 2 textbox 3 label 1 button ile yapıldı 2 sayıyı toplatan program
+            //int s1, s2, sonuc = 0;
+            //s1 = Convert.ToInt32(txtsayı1.Text);
+            //s2 = Convert.ToInt32(txtsayı2.Text);
+            //sonuc = s1 + s2;
+            //lblsonuc.Text = "sonuç=" + sonuc;
+
+
+            // evet bu programda final vize ayarlama ve kaldığında kırmızı geçtiğinde yeşil
+            // veren program
+            double vize, final, ortalama;
+            vize = Convert.ToDouble(txtvize.Text);
+            final = Convert.ToDouble(txtfinal.Text);
+            ortalama = vize * 0.4 + final * 0.6;
+            lblorta.Text = ortalama.ToString();
+            if (ortalama<50|| final<50)
+            {
+                lbldurum.Text = "kaldınız";
+                this.BackColor = Color.Red;
+            }
+            else
+            {
+                lbldurum.Text = "geçtiniz";
+                this.BackColor = Color.Green;
+            }
+        }
+    }
+}
